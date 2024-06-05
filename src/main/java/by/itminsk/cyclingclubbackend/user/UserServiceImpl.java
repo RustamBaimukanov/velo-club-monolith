2293e4856ec09ce70df0703fb6b84d8c2419b0b9
Команда джавистов <watashirustam@gmail.com>
2024-06-05 10:08:35 +0300

    fix register form

--- src/main/java/by/itminsk/cyclingclubbackend/user/UserServiceImpl.java
+++ src/main/java/by/itminsk/cyclingclubbackend/user/UserServiceImpl.java
@@ -328,7 +328,7 @@ public class UserServiceImpl implements UserService {
             user.setLastName(registerDto.getLastName());
             user.setBirthDate(registerDto.getBirth());
             user.setSex(registerDto.getGender());
-            user.setCity(cityService.getCity(registerDto.getCity()));
+            user.setCity(City.builder().id(registerDto.getCity()).build());
             user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
             Role role = roleRepository.findRoleByName(RoleEnum.DABBLER);
             user.setRole(role);
