ace2559e34bf7506be3190041976d6bd4393247a
Rustam <watashirustam@gmail.com>
2024-10-24 12:00:19 +0300

    fix

--- src/main/java/by/itminsk/cyclingclubbackend/controller/survey/VoteController.java
+++ src/main/java/by/itminsk/cyclingclubbackend/controller/survey/VoteController.java
@@ -17,6 +17,7 @@ public class VoteController {
 
     private final UserService userService;
 
+
     @Operation(
             summary = "Проголосовать за выбранный ответ")
     @PostMapping("{id}")
